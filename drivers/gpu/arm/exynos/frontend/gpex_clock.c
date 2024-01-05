071d0ad6ff8aa356ad85c985325637df8284bd3e
snapdragon teaks <inutilidades639@gmail.com>
2024-01-05 04:46:26 -0300

    Revert "mali/exynos: add voltage control"
    This reverts commit 329a28aa1cf12bc6822b7f7f5143e87881cd6401.

--- drivers/gpu/arm/exynos/frontend/gpex_clock.c
+++ drivers/gpu/arm/exynos/frontend/gpex_clock.c
@@ -76,7 +76,7 @@ u64 gpex_clock_get_time_busy(int level)
 /*******************************************
  * static helper functions
  ******************************************/
-int gpex_clock_update_config_data_from_dt()
+static int gpex_clock_update_config_data_from_dt()
 {
 	int ret = 0;
 	struct freq_volt *fv_array;
@@ -121,9 +121,7 @@ int gpex_clock_update_config_data_from_dt()
 
 	return 0;
 }
-/*******************************************
- * static helper functions
- ******************************************/
+
 static int set_clock_using_calapi(int clk)
 {
 	int ret = 0;
