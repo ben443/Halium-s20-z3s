ae012653e74fffc5abe8bdb21f01e100f99be067
snapdragon teaks <inutilidades639@gmail.com>
2024-01-05 04:48:00 -0300

    Revert "drivers: soc: add new G3D changes"
    This reverts commit 1c8bdb5351e5e36697681d29240fdb5e4a20f56c.

--- drivers/soc/samsung/cal-if/pmucal_local.c
+++ drivers/soc/samsung/cal-if/pmucal_local.c
@@ -17,7 +17,7 @@ struct pmucal_pd *pmucal_blkpwr_list[PMUCAL_NUM_PDS];
  */
 int pmucal_local_enable(unsigned int pd_id)
 {
-	int ret, i;
+	int ret = 0;
 
 	dbg_snapshot_pmu(pd_id, __func__, DSS_FLAG_IN);
 
@@ -40,13 +40,6 @@ int pmucal_local_enable(unsigned int pd_id)
 	if (ret) {
 		pr_err("%s %s: error on handling enable sequence. (pd_id : %d)\n",
 				PMUCAL_PREFIX, __func__, pd_id);
-		if (pd_id == 8) { /* for G3D */
-			for (i = 0; i < pmucal_pd_list[pd_id].num_save; i++) {
-				pr_err("%s[0x%x] = 0x%x\n", pmucal_pd_list[pd_id].save[i].sfr_name,
-						pmucal_pd_list[pd_id].save[i].offset,
-						__raw_readl(pmucal_pd_list[pd_id].save[i].base_va +
-							pmucal_pd_list[pd_id].save[i].offset));
-			}
 		goto err_out;
 	}
 
